3f3b874ce83a7cce950ce2de20afaf0b43d1e03f
5sbread <oohogong@gmail.com>
2022-08-30 15:41:42 +0900

    수정 허라

--- src/main/webapp/resources/js/dom_3_ex1.js
+++ src/main/webapp/resources/js/dom_3_ex1.js
@@ -4,7 +4,11 @@ const d3 = document.getElementById("d3");
 const btn = document.getElementById("btn");
 const btn2 = document.getElementById("btn2");
 const btn3 = document.getElementById("btn3");
+const btn4 = document.getElementById("btn4");
+const select = document.getElementById("select");
+const c1 = document.getElementsByClassName("c1");
 
+//이벤트 등록
 btn.addEventListener("click", function(){
     console.log("work");
     console.log(d1.value);
@@ -21,4 +25,34 @@ btn3.addEventListener("click", function(){
     
     //이벤트 강제로 발생
     btn2.click();
+});
+
+//===========================================
+btn.addEventListener("click", function(){
+    console.log("operator : ", select.value);
+    let result = 0;
+    
+    if(select.value=="+"){
+        result = d1.value*1 + d2.value*1;
+    }else if(select.value=="-"){
+        result = d1.value*1 - d2.value*1;
+    }else if(select.value=="*"){
+        result = d1.value*1 * d2.value*1;
+    }else if(select.value=="/"){
+        result = d1.value*1 / d2.value*1;
+    }else {
+        result = d1.value*1 % d2.value*1;
+    }
+    d3.value = result;
+})
+
+//===========================================
+btn4.addEventListener("click", function(){
+    alert(c1);
+    console.log(c1);
+    console.log(cq.value);
+
+    for(let i=0; i<c1.length; i++){
+        console.log(c1[i].selected);
+    }
 });
