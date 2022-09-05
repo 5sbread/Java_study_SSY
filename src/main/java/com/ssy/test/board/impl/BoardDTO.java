c0394688c47e009728ff403f1d84cf8bc0547791
5sbread <oohogong@gmail.com>
2022-09-06 02:42:13 +0900

    오류 수정

--- src/main/java/com/ssy/test/board/impl/BoardDTO.java
+++ src/main/java/com/ssy/test/board/impl/BoardDTO.java
@@ -3,62 +3,32 @@ package com.ssy.test.board.impl;
 import java.sql.Date;
 import java.util.List;
 
-public class BoardDTO {
+public class BoardDTO extends CommentDTO {
 	
 	//notice / qna 공통 dto
 	
-	private Long num;
 	private String title;
-	private String writer;
-	private String contents;
-	private Date regDate;
-	private Long hit;
-	private List<BoardFileDTO> boardFileDTOs;
+	private Long het;
+	private List<BoardFileDTO> boardFileDTO;
 	
-	
-	public List<BoardFileDTO> getBoardFileDTOs() {
-		return boardFileDTOs;
-	}
-	public void setBoardFileDTOs(List<BoardFileDTO> boardFileDTOs) {
-		this.boardFileDTOs = boardFileDTOs;
-	}
-	public Long getNum() {
-		return num;
-	}
-	public void setNum(Long num) {
-		this.num = num;
-	}
 	public String getTitle() {
 		return title;
 	}
 	public void setTitle(String title) {
 		this.title = title;
 	}
-	public String getWriter() {
-		return writer;
+	public Long getHet() {
+		return het;
 	}
-	public void setWriter(String writer) {
-		this.writer = writer;
+	public void setHet(Long het) {
+		this.het = het;
 	}
-	public String getContents() {
-		return contents;
+	public List<BoardFileDTO> getBoardFileDTO() {
+		return boardFileDTO;
 	}
-	public void setContents(String contents) {
-		this.contents = contents;
+	public void setBoardFileDTO(List<BoardFileDTO> boardFileDTO) {
+		this.boardFileDTO = boardFileDTO;
 	}
-	public Date getRegDate() {
-		return regDate;
-	}
-	public void setRegDate(Date regDate) {
-		this.regDate = regDate;
-	}
-	public Long getHit() {
-		return hit;
-	}
-	public void setHit(Long hit) {
-		this.hit = hit;
-	}
-	
 	
 	
 
