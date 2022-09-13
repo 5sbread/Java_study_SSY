c37c67e03aa487608bb58ce970a0ae3056c163d8
5sbread <oohogong@gmail.com>
2022-09-13 17:50:55 +0900

    EXECPTION

---
+++ src/main/java/com/ssy/test/error/FrontErrorController.java
@@ -0,0 +1,13 @@
+package com.ssy.test.error;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/error/*")
+public class FrontErrorController {
+	
+	public ModelAndView error404                                                                                                                                                                                                                                                 
+
+}
